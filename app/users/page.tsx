cc862476014a4a67505b9be9edcb49ba8b592ed6
Birhanu <birhanuasmamaw24@gmail.com>
2024-03-25 23:54:29 -0900

    Dynamic route

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import UserTable from './UserTable';
 
 interface User{
   id: number;
@@ -7,27 +8,12 @@ interface User{
 }
 
 const UsersPage = async() =>  {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users: User[] = await res.json();
+
 
   return (
     <>
     <h1>Users</h1>
-    <table className='table table-bordered' >  {/* daisy ui */}
-    
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map(user => <tr key={user.id}>
-          <td>{user.name}</td>
-          <td>{user.email}</td></tr> )}
-      </tbody>
-      
-    </table>
+    <UserTable/>
     </>
   )
 }
