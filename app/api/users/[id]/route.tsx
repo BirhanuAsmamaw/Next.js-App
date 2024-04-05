7d41e0707325f2e9741212d55fb3b44f0015b31d
Birhanu <birhanuasmamaw24@gmail.com>
2024-04-05 05:05:40 -0900

    pushing all the changes I made

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -7,3 +7,20 @@ export function GET( request: NextRequest,
       return NextResponse.json({error : 'User not found'}, {status: 404} )
     return NextResponse.json({id: 1, name: 'Birhanu' });
     }
+
+    export async function PUT(request: NextRequest, 
+       {params}: {params: {id: number} }){
+       //Validate the request body
+       //if invalid, return 400
+       const body = await request.json();
+       if (!body.name)
+         return NextResponse.json({error: "Name is Required "} , {status: 400})
+
+       //Fetch the user with the given id
+       // If doesn't exist, return 404
+       if (params.id > 10)
+          return NextResponse.json({error: 'user not found '},{status : 404})
+
+       //Update the user 
+       //return the updated user
+       return NextResponse.json({id: 1, name: body.name})}
--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -6,7 +6,7 @@ const AddToCart = () => {
     <div>
         <button className='btn btn-primary' onClick={() => console.log("click")}>Add to Cart</button>
     </div>
-  )
+  ) 
 }
 
 export default AddToCart
